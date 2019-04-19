2f9667263042439dcf5f85a1440eaf5c680d2836
MatteoMagri <49791899+MatteoMagri@users.noreply.github.com>
2019-04-19 19:08:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
   url = 'https://github.com/MatteoMagri/Mandelpy',   
   download_url = 'https://github.com/MatteoMagri/Mandelpy/archive/Mandelbrot.tar.gz',
   keywords = ['Python', 'Mandelbrot', 'PIL', 'ART', 'Graphics'],   
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[            
+          'Pillow',
+          'Scipy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      
 
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
 
     'License :: OSI Approved :: MIT License',   
