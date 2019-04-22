bfc5bb9f1f179a30e5f1dc782d15b92a9d51bff4
MatteoMagri <49791899+MatteoMagri@users.noreply.github.com>
2019-04-22 19:01:14 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mandelpy',         
   packages = ['mandelpy'],   
-  version = '1.22',      
+  version = '1.3',      
   license='MIT',        
   description = 'Mandelbrot library for Python',   
   author = 'Matteo Magri',                   
   author_email = 'magri.matteo@outlook.com',      
   url = 'https://github.com/MatteoMagri/Mandelpy',   
-  download_url = 'https://github.com/MatteoMagri/Mandelpy/archive/v1.22.tar.gz',
+  download_url = 'https://github.com/MatteoMagri/Mandelpy/archive/v1.3.tar.gz',
   keywords = ['Python', 'Mandelbrot', 'PIL', 'ART', 'Graphics'],   
   install_requires=[            
           'Pillow',
@@ -17,7 +17,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      
 
-    'Intended Audience :: Developers',     
+    'Intended Audience :: Artist',     
     'Topic :: Software Development :: Build Tools',
 
     'License :: OSI Approved :: MIT License',   
